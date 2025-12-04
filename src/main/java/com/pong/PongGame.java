4032de515a3e2e4abb2253977de8007901aeb7e4
juniverse19 <ryanjkim18@gmail.com>
2025-12-04 04:12:26 +0000

    a

--- src/main/java/com/pong/PongGame.java
+++ src/main/java/com/pong/PongGame.java
@@ -1,9 +1,18 @@
+//
+//  Class author:  Ryan K
+//  Date created:  12/1/25
+//  General description: A pong game in which the player plays pong against an AI.
+//
+
+
 package com.pong;
+
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+
 
 public class PongGame extends JPanel implements MouseMotionListener {
     static int width = 640; // this is the amount of pixels to the right side of the screen
@@ -18,7 +27,9 @@ public class PongGame extends JPanel implements MouseMotionListener {
     private Speedup fast;
     private SlowDown slow;
 
+
     public PongGame() {
+
 
         aiPaddle = new Paddle(610, 240, 50, 9, Color.WHITE);
         JLabel pScore = new JLabel("0");
@@ -31,11 +42,13 @@ public class PongGame extends JPanel implements MouseMotionListener {
         addMouseMotionListener(this);
         ball = new Ball(200, 200, 10, 3, Color.RED, 10);
 
+
         //create any other objects necessary to play the game.
         playerPaddle = new Paddle(30, 240, 50, 9, Color.WHITE);
         fast = new Speedup(100, 100, 50, 50);
         slow = new SlowDown(150, 200, 50, 50);
     }
+
 
     // precondition: None
     // postcondition: returns playerScore
@@ -43,40 +56,46 @@ public class PongGame extends JPanel implements MouseMotionListener {
         return playerScore;
     }
 
+
     // precondition: None
     // postcondition: returns aiScore
     public int getAiScore() {
         return aiScore;
     }
 
-    //precondition: All visual components are initialized, non-null, objects 
+
+    //precondition: All visual components are initialized, non-null, objects
     //postcondition: A frame of the game is drawn onto the screen.
     public void paintComponent(Graphics g) {
+
 
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, width, height);
 
+
         g.setColor(Color.WHITE);
         g.drawString("The Score is User:" + playerScore + " vs Ai:" + aiScore, 240, 20);
-        
+       
         aiPaddle.draw(g);
-        
+       
         //call the "draw" function of any visual component you'd like to show up on the screen.
         playerPaddle.draw(g);
         fast.draw(g);
         slow.draw(g);
-        
+       
         ball.draw(g);
     }
+
 
     // precondition: all required visual components are intialized to non-null
     // values
     // postcondition: one frame of the game is "played"
     public void gameLogic() {
         //add commands here to make the game play propperly
-        
+       
         aiPaddle.moveY(ball.getY());
         playerPaddle.moveY(userMouseY);
+
 
         if (aiPaddle.isTouching(ball)) {
            ball.reverseX();
@@ -84,17 +103,23 @@ public class PongGame extends JPanel implements MouseMotionListener {
         if (playerPaddle.isTouching(ball)) {
            ball.reverseX();
         }
-        
+       
+        if (fast.isTouching(ball)) {
+    ball.setChangeX(ball.getChangeX() * 1.2);
+    ball.setChangey(ball.getChangeY() * 1.2);
+    fast = new Speedup(-100, -100, 0, 0);
+}
         ball.moveBall();
+
 
         ball.bounceOffwalls(0, height-20);
 
-        if(fast.isTouching(ball)){
 
-        }
         pointScored();
 
+
     }
+
 
     // precondition: ball is a non-null object that exists in the world
     // postcondition: determines if either ai or the player score needs to be
@@ -114,7 +139,9 @@ public class PongGame extends JPanel implements MouseMotionListener {
                 ball.sety(height/2);
             }
 
+
     }
+
 
     // you do not need to edit the below methods, but please do not remove them as
     // they are required for the program to run.
@@ -122,7 +149,9 @@ public class PongGame extends JPanel implements MouseMotionListener {
     public void mouseDragged(MouseEvent e) {
         // TODO Auto-generated method stub
 
+
     }
+
 
     @Override
     public void mouseMoved(MouseEvent e) {
@@ -130,4 +159,6 @@ public class PongGame extends JPanel implements MouseMotionListener {
         userMouseY = e.getY();
     }
 
+
 }
+
